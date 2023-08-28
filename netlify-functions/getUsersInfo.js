51789e4de3b4eecbfa49b07e998df8354e5fddb3
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-08-28 15:27:15 +0300

    test

--- netlify-functions/getUsersInfo.js
+++ netlify-functions/getUsersInfo.js
@@ -3,6 +3,7 @@ import fetch from "node-fetch";
 export async function handler(event, context) {
   try {
     const response = await fetch("https://joyful-sunshine-d424f0.netlify.app/api/users/");
+    console.log("users.................");
     const data = await response.json();
 
     return {
